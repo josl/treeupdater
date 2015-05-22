e6bcb569a57c16875dacf51e69993f57b83ad607
Jose Luis Bellod Cisneros <bellod.cisneros@gmail.com>
2015-05-22 12:06:55 +0200

    Parsing and updating the tree implemented

--- index.js
+++ index.js
@@ -1,4 +1,45 @@
 'use strict';
-module.exports = function (str) {
-  console.log(str || 'Rainbow');
+
+
+module.exports = function () {
+  var parser = require('biojs-io-newick');
+  var fs = require('fs');
+  return {
+      readTreeFile: function(tree) {
+        fs.readFile(tree, 'utf8', function (err, data) {
+          if (err) {
+            return err;
+          }
+          return this.parseTree(data);
+        });
+      },
+      matchFromFile: function(file){
+        fs.readFile(file, 'utf8', function (err, data) {
+          if (err) {
+            return err;
+          }
+          return this.returnMatch(data);
+        });
+      },
+      parseTree: function(string){
+        return parser.parse_newick(string);
+      },
+      returnMatch: function(string){
+        return JSON.parse(string);
+      },
+      updateTree: function test (tree, match){
+        function trans(tree, match){
+          if (tree.name === match.match){ // End recursion
+            tree.children = [{name: match.name, branch_length: match.snps}];
+          }else{ // Recursive
+            if (tree.children){
+              tree.children.forEach(function(son){
+                return trans(son, match);
+              });
+            }
+          }
+        }
+        return trans(tree, match);
+      }
+  };
 };
--- test/test.js
+++ test/test.js
@@ -3,8 +3,41 @@ var assert = require('assert');
 var treeupdater = require('../');
 
 describe('treeupdater node module', function () {
-  it('must have at least one test', function () {
-    treeupdater();
-    assert(false, 'I was too lazy to write any tests. Shame on me.');
+  it ('match should be C', function () {
+    //var tree = treeupdater().parseTree('(A:0.1,B:0.2,(C:0.3,D:0.4)E:0.5)F');
+    var match = treeupdater().returnMatch('{'+
+      '"N": [1,1,3,4,4],'+
+      '"match": "C",'+
+      '"snps": 0.5'+
+    '}');
+    assert.equal(match.match, 'C');
   });
+
+  it ('must return JSON tree', function(){
+    var tree = treeupdater().parseTree('(A:0.1,B:0.2,(C:0.3,D:0.4)E:0.5)F');
+    assert.equal(tree['name'], 'F');
+    assert.equal(
+      JSON.stringify(tree['children'][0]),
+      JSON.stringify({name: "A", branch_length: 0.1})
+    );
+    assert.equal(
+      JSON.stringify(tree['children'][1]),
+      JSON.stringify({name: "B", branch_length: 0.2})
+    );
+    assert.equal(tree['children'][2].name, 'E');
+  });
+
+  it ('must update tree with proper match', function(){
+    var match = treeupdater().returnMatch('{'+
+      '"N": [1,1,3,4,4],'+
+      '"match": "C",'+
+      '"snps": 0.5,'+
+      '"name": "H"'+
+    '}');
+    var tree = treeupdater().parseTree('(A:0.1,B:0.2,(C:0.3,D:0.4)E:0.5)F');
+    treeupdater().updateTree(tree, match);
+    assert.equal(tree.children[2].children[0].children[0].name, "H");
+    assert.equal(tree.children[2].children[0].children[0].branch_length, 0.5);
+  });
+
 });
